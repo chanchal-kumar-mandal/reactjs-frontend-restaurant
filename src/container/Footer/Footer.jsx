ada2b46398936a7a6717f746b409749fc6fa19cf
chanchal-kumar-mandal <ckmandal9@gmail.com>
2022-04-13 13:18:25 +0530

    advanced modern Restaurant Frontend application on reactjs

---
+++ src/container/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FiFacebook, FiInstagram, FiTwitter } from 'react-icons/fi';
+
+import { FooterOverlay, Newsletter } from '../../components';
+import './Footer.css';
+
+const Footer = () => (
+  <div className="app__footer section__padding">
+    <FooterOverlay />
+    <Newsletter />
+  </div>
+);
+
+export default Footer;
